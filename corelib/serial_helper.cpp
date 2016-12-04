31642d6b9a7f203ba7e5202b8afadcc2d1f0c5ab
Guillaume227 <guillaume.giraud@gmail.com>
2016-12-04 09:39:04 +0100

    scindage de main_test.cpp

---
+++ corelib/serial_helper.cpp
@@ -0,0 +1,128 @@
+#include "serial_helper.h"
+#include "../RS-232/rs232.h"
+#include <iostream>
+#include <iomanip>
+#include <thread>
+#include <exception>
+
+void print_hex(int c)
+{
+	std::ios::fmtflags f(std::cout.flags());
+
+	auto cc = static_cast<uchar>(c);
+	if (cc == '\'' || cc == '\n')
+		std::cout << cc;
+	else
+		std::cout << "\\x" << std::setw(2) << std::setfill('0') << std::hex << (int)cc;
+
+	std::cout.flags(f);
+}
+
+
+CSerialHelper::CSerialHelper(int port_num, int baud_rate) : cport_nr(port_num)
+{
+
+	for (int port = 0; port < 5; port++)
+	{
+		if (!RS232_OpenComport(port, baud_rate, mode))
+		{
+			cport_nr = port;
+			std::cout << "using COM" << port + 1 << " at " << baud_rate << " bps" << std::endl << std::endl;
+			return;
+		}
+	}
+
+	throw std::exception("Can not open comport");
+}
+
+void CSerialHelper::putc(int c)
+{
+	RS232_SendByte(cport_nr, (char)c);
+}
+
+CSerialHelper::~CSerialHelper() {
+	RS232_CloseComport(cport_nr);
+}
+
+
+
+
+
+CSerialParser::CSerialParser() :
+	SerialParser<CSerialHelper>(ser_helper)
+{
+	buf_.resize(256, 0);
+
+	std::thread([&]() {
+
+		while (_run_thread_flag)
+		{
+			int n = RS232_PollComport(ser_helper.cport_nr, &buf_[0], buf_.size());
+
+			if (n > 0)
+			{
+				printf("received %i bytes: %s\n", n, (char *)&buf_[0]);
+
+				for (int i = 0; i < n; i++)
+				{
+					receivec(buf_[i]);
+				}
+			}
+
+			std::this_thread::yield();
+		}
+
+	}).detach();
+}
+
+void CSerialParser::receivec(int c)
+{
+	//printf("char:{%c}",static_cast<char>(c));
+	if (static_cast<char>(c) == delimiter_end_)
+	{
+		rx_buffer_.push_back(delimiter_end_);
+
+		do
+		{
+			auto msgType = parseMsg();
+			if (msgType == MsgType::NONE)
+			{
+				std::string msg((char*)&rx_buffer_[0], rx_buffer_.size());
+				printf("can't parse; keeping %u bytes: %s\n", rx_buffer_.size(), msg.c_str());
+				break;
+			}
+			else {
+				stats[static_cast<int>(msgType)]++;
+
+				if (print_rcvd_ || static_cast<int>(msgType) >= static_cast<int>(MsgType::Status))
+				{
+					std::cout << "recvd " << msgType << " ";
+					motorCmdData.printVal(std::cout, msgType) << std::endl;
+				}
+				/*
+				if (msgType == MsgType::Status)
+				if (motorCmdData.Status == 0)
+				{
+				motorCmdData.Status = 1;
+				sendMsg(MsgType::Status);
+				}
+				*/
+			}
+
+		} while (!rx_buffer_.empty());
+	}
+	else
+	{
+		rx_buffer_.push_back(c);
+	}
+}
+
+void CSerialParser::print_stats() const
+{
+	std::cout << std::endl << "====Stats====" << std::endl;
+	for (int i = 0; i < static_cast<int>(MsgType::NONE); i++)
+	{
+		if (stats[i])
+			std::cout << static_cast<MsgType>(i) << ": " << stats[i] << std::endl;
+	}
+}
---
+++ corelib/serial_helper.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "SerialParser.h"
+
+void print_hex(int c);
+
+struct CSerialHelper
+{
+	int cport_nr;        /* 0 <-> /dev/ttyS0 (COM1 on windows) : starts with 0 */
+
+	char mode[4] = { '8','N','1',0 };
+
+	CSerialHelper(int port_num, int baud_rate);
+
+	void putc(int c);
+
+	~CSerialHelper();
+};
+
+struct CSerialParser : public SerialParser<CSerialHelper>
+{
+	CSerialHelper ser_helper{ 0, BAUD_RATE };
+
+	std::vector<uchar> buf_;
+
+	int stats[static_cast<int>(MsgType::NONE)] = { 0 };
+	bool print_rcvd_ = false;
+	volatile bool _run_thread_flag{ true };
+
+	CSerialParser();
+	~CSerialParser() { _run_thread_flag = false; }
+
+	void receivec(int c);
+	void putc(int c)
+	{
+		print_hex(c);
+		serial_helper_.putc(c);
+		//receivec(c);
+	}
+
+	void print_stats() const;
+};
